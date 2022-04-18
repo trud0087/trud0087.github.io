28ddce58e21942f43b9b11ada2f176af2ee2b7e5
trud0087 <trud0087@algonquinlive.com>
2022-04-18 15:32:14 -0400

    update

--- lib2024/final/js/scripts.js
+++ lib2024/final/js/scripts.js
@@ -1,8 +1,6 @@
-<html>
-<body>
-<script>  
-// It is single line comment  
-document.write("Hello Javascript");  
-</script> 
-</body>
-</html>
+ // It is single line comment  
+document.index("Hello Javascript");  
+/* It is multi line comment.  
+It will not be displayed */  
+document.index("Example of Javascript multiline comment");  
+
